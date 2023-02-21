4d244cefad9180a85f52e1d4dd7b4b63af841503
Md Rasel <97774086+MdRasel5@users.noreply.github.com>
2023-02-21 23:25:35 +0600

    responsive done, add animation

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-// var t1 = new TimelineMax({
-//   paused: true
-// });
+var t1 = new TimelineMax({
+    paused: true
+});
 
-// t1.to(
-//   ".left-navbar-menus",
-//   .7, {
-//     width: "25%",
-//     ease: Expo.easeInOut,
-//   },
-//   "-=.6"
-// );
+t1.to(
+    ".left-navbar",
+    .7, {
+        width: "25%",
+        ease: Expo.easeInOut,
+    },
+    "-=.6"
+);
 
-// // overlay
-// t1.to(
-//   ".left-navbar-overlay",
-//   .7, {
-//     width: "75%",
-//     ease: Expo.easeInOut,
-//     delay: -1,
-//   },
-//   "-=.5"
-// );
+// overlay
+t1.to(
+    ".left-navbar-overlay",
+    .7, {
+        width: "75%",
+        ease: Expo.easeInOut,
+        // delay: -1,
+    },
+    "-=.5"
+);
 
-// t1.reverse();
-// $(document).on("click", ".toggle-btn", function () {
-//   t1.reversed(!t1.reversed());
-// });
-// $(document).on("click", ".menu-text", function () {
-//   t1.reversed(!t1.reversed());
-// });
-// $(document).on("click", ".left-navbar-overlay", function () {
-//   t1.reversed(!t1.reversed());
-// });
+t1.reverse();
+$(document).on("click", ".toggle-btn", function () {
+    t1.reversed(!t1.reversed());
+});
+$(document).on("click", ".menu-text", function () {
+    t1.reversed(!t1.reversed());
+});
+$(document).on("click", ".left-navbar-overlay", function () {
+    t1.reversed(!t1.reversed());
+});
